e049ae0cf1a42ffaffb797027b50efc331e7b21a
unknown <lixurui@upoa.com>
2017-06-09 17:32:46 +0800

    保存Fraud成功

---
+++ Data_Preprocess_Spark2_11/src/PropMap/Load_IndexerPipeLine.scala
@@ -0,0 +1,86 @@
+package PropMap
+import org.apache.log4j.Level
+import org.apache.log4j.Logger
+import org.apache.spark._
+import org.apache.spark.sql._
+import org.apache.spark.sql.types._
+import org.apache.spark.SparkContext._
+import org.apache.log4j.{Level, Logger}
+import org.apache.spark.sql.SparkSession
+import org.apache.log4j.Level
+import org.apache.log4j.Logger
+import org.apache.spark.graphx._
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.SQLContext
+import org.apache.spark.sql.hive.HiveContext
+import scala.collection.mutable.MutableList
+import scala.Range
+import org.apache.spark.graphx._
+import org.apache.spark.rdd.RDD
+import scala.collection.mutable.{Buffer,Set,Map}
+import org.apache.spark.rdd.RDD.rddToPairRDDFunctions
+import org.apache.spark.sql.functions._
+import org.apache.spark.storage.StorageLevel
+import org.apache.spark.ml.feature.StandardScaler
+import org.apache.spark.ml.feature.MinMaxScaler
+import org.apache.spark.ml.feature.Normalizer
+import org.apache.spark.ml.feature.StringIndexer
+import org.apache.spark.ml.feature.VectorAssembler
+import org.apache.spark.ml.feature.OneHotEncoder
+import org.apache.spark.ml.clustering.KMeans
+import org.apache.spark.ml.Pipeline
+import org.apache.spark.ml.classification._
+import org.apache.spark.ml.evaluation.MulticlassClassificationEvaluator
+import org.apache.spark.ml.feature.{IndexToString, StringIndexer, VectorAssembler}
+import scala.collection.mutable.ArrayBuffer
+import scala.reflect.runtime.universe
+import org.apache.spark.ml.Pipeline
+import org.apache.spark.ml.PipelineStage
+import org.apache.spark.ml.PipelineModel
+
+
+object Load_IndexerPipeLine {
+  
+
+  def main(args: Array[String]): Unit = {
+
+    //屏蔽日志
+    Logger.getLogger("org").setLevel(Level.ERROR);
+    Logger.getLogger("akka").setLevel(Level.ERROR);
+    Logger.getLogger("hive").setLevel(Level.WARN);
+    Logger.getLogger("parse").setLevel(Level.ERROR); 
+    
+    //val sparkConf = new SparkConf().setAppName("spark2SQL")
+    val warehouseLocation = "spark-warehouse"
+    
+    val ss = SparkSession
+      .builder()
+      .appName("Save_IndexerPipeLine")
+      .config("spark.sql.warehouse.dir", warehouseLocation)
+      .config("hive.metastore.schema.verification", false)
+      .getOrCreate()
+  
+    import ss.implicits._
+    import ss.sql
+ 
+    val startTime = System.currentTimeMillis(); 
+     
+    val startdate = "20160701"
+    val enddate = "20160701"
+    var testData = IntelUtil.get_from_HDFS.get_filled_DF(ss, startdate, enddate).repartition(5000).persist(StorageLevel.MEMORY_AND_DISK_SER)// .cache         //.persist(StorageLevel.MEMORY_AND_DISK_SER)//
+    testData.show(5)
+    println("testData done in " + (System.currentTimeMillis()-startTime)/(1000*60) + " minutes." )
+      
+    val my_index_Model = PipelineModel.load("xrli/IntelDNN/index_Model")
+    println("Load pipeline done in " + (System.currentTimeMillis()-startTime)/(1000*60) + " minutes." )
+     
+    println("start transform data!")
+    var vec_data = my_index_Model.transform(testData)
+    println("Indexed done in " + (System.currentTimeMillis()-startTime)/(1000*60) + " minutes." )    
+    vec_data.show(5)
+  
+    println("All done in " + (System.currentTimeMillis()-startTime)/(1000*60) + " minutes." )   
+  }
+  
+    
+}
